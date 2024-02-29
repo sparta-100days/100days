bbd49268f69af55c64a45d4cb08014ecbc7fbf95
YJ9352 <act07612@gmail.com>
2024-02-29 10:45:20 +0900

    🏷️Style: 쓰지 않는 import 정리

--- src/main/kotlin/com/example/days/domain/user/dto/request/SignUpRequest.kt
+++ src/main/kotlin/com/example/days/domain/user/dto/request/SignUpRequest.kt
@@ -1,6 +1,5 @@
 package com.example.days.domain.user.dto.request
 
-import com.example.days.domain.user.model.User
 import java.time.LocalDate
 
 data class SignUpRequest(
--- src/main/kotlin/com/example/days/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/com/example/days/domain/user/repository/UserRepository.kt
@@ -1,7 +1,5 @@
 package com.example.days.domain.user.repository
 
-import com.example.days.domain.user.dto.request.UserRequest
-import com.example.days.domain.user.dto.response.SignUpResponse
 import com.example.days.domain.user.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 
