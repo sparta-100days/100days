a7f7f92e0b76284eac0020302da5eaa664440f46
YJ9352 <act07612@gmail.com>
2024-03-04 02:11:37 +0900

    🏷️Style: 사용하지 않는 import 삭제

--- src/main/kotlin/com/example/days/domain/user/dto/request/SignUpRequest.kt
+++ src/main/kotlin/com/example/days/domain/user/dto/request/SignUpRequest.kt
@@ -2,7 +2,6 @@ package com.example.days.domain.user.dto.request
 
 import com.example.days.domain.user.model.UserRole
 import com.example.days.domain.user.model.UserStatus
-import io.swagger.v3.oas.annotations.Hidden
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Pattern
