c44a81f5182623743b2912341656cb88199ac814
YJ9352 <act07612@gmail.com>
2024-03-04 05:42:59 +0900

    🏷️Style: 쓰지 않는 import 삭제

--- src/main/kotlin/com/example/days/domain/user/dto/response/LoginResponse.kt
+++ src/main/kotlin/com/example/days/domain/user/dto/response/LoginResponse.kt
@@ -1,8 +1,5 @@
 package com.example.days.domain.user.dto.response
 
-import com.example.days.domain.user.model.User
-import com.example.days.global.infra.security.jwt.JwtPlugin
-
 data class LoginResponse(
     val accessToken: String,
     val nickName: String,
--- src/main/kotlin/com/example/days/global/infra/security/jwt/JwtPlugin.kt
+++ src/main/kotlin/com/example/days/global/infra/security/jwt/JwtPlugin.kt
@@ -10,7 +10,6 @@ import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.nio.charset.StandardCharsets
-import java.security.KeyRep.Type
 import java.time.Duration
 import java.time.Instant
 import java.util.*
