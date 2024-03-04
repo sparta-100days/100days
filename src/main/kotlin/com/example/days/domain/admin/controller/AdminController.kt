7618acf5282d6fb6a2e15e67937c5e1fd54b912c
rim109 <fla7722@naver.com>
2024-03-04 20:20:16 +0900

    🐛Fix: 경로수정
    -admins s로 붙여서 변경

--- src/main/kotlin/com/example/days/domain/admin/controller/AdminController.kt
+++ src/main/kotlin/com/example/days/domain/admin/controller/AdminController.kt
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/admins")
 class AdminController(
     private val adminService: AdminService
 ) {
