65cd6f1eb6788f8be564d20214f7a28276fc0800
rim109 <fla7722@naver.com>
2024-03-04 20:16:38 +0900

    🐛Fix: AdminCustomRepository
    -순환참조 에러 해결

--- src/main/kotlin/com/example/days/domain/admin/repository/AdminRepository.kt
+++ src/main/kotlin/com/example/days/domain/admin/repository/AdminRepository.kt
@@ -3,7 +3,7 @@ package com.example.days.domain.admin.repository
 import com.example.days.domain.admin.model.Admin
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface AdminRepository : JpaRepository<Admin, Long> {
+interface AdminRepository : JpaRepository<Admin, Long>, AdminCustomRepository {
     fun existsByEmail(email: String): Boolean
 
     fun existsByNickname(nickname: String): Boolean
