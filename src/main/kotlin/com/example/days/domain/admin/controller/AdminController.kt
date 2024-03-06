3740eef1b9568482c5e298b3503fc829d98fb586
rim109 <fla7722@naver.com>
2024-03-06 20:53:06 +0900

    🐛Fix: 경로 수정
    -/api 붙임.
    - 통일성을 위해서!

--- src/main/kotlin/com/example/days/domain/admin/controller/AdminController.kt
+++ src/main/kotlin/com/example/days/domain/admin/controller/AdminController.kt
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("api/admins")
+@RequestMapping("/api/admins")
 class AdminController(
     private val adminService: AdminService
 ) {
