ec325fe713a283a9f571fea8c6abd75519cab8cf
rim109 <fla7722@naver.com>
2024-03-09 22:40:59 +0900

    ⌛Refactor: response 수정
    -신고 개수 추가
    -신고된 유저 상태 삭제

--- src/main/kotlin/com/example/days/domain/report/dto/response/UserReportResponse.kt
+++ src/main/kotlin/com/example/days/domain/report/dto/response/UserReportResponse.kt
@@ -1,21 +1,20 @@
 package com.example.days.domain.report.dto.response
 
 import com.example.days.domain.report.model.UserReport
-import com.example.days.domain.user.model.Status
 
 data class UserReportResponse(
     val id: Long,
     val reportedUserNickname: String,
-    val reportedUserStatus: Status,
-    val content: String
+    val content: String,
+    val countReport: Int
 ) {
     companion object {
         fun from(userReport: UserReport): UserReportResponse {
             return UserReportResponse(
                 id = userReport.id!!,
                 content = userReport.content,
-                reportedUserStatus = userReport.reportedUserId.status,
-                reportedUserNickname = userReport.reportedUserId.nickname
+                reportedUserNickname = userReport.reportedUserId.nickname,
+                countReport = userReport.reportedUserId.countReport
             )
         }
     }
