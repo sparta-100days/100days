0a5fd72db54416a1d8b1a0940b4ba8e76640320c
YJ9352 <act07612@gmail.com>
2024-03-16 03:33:18 +0900

    🏷️Style: 사용하지 않는 코드 삭제

--- src/main/kotlin/com/example/days/global/infra/security/jwt/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/example/days/global/infra/security/jwt/JwtAuthenticationFilter.kt
@@ -5,7 +5,6 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
