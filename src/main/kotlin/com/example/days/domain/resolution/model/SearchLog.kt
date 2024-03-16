0b522ed01a8519fa69f75ea98a25c15c9289894a
rim109 <fla7722@naver.com>
2024-03-16 15:45:48 +0900

    ⭐Feat: searchLog 엔티티 추가

---
+++ src/main/kotlin/com/example/days/domain/resolution/model/SearchLog.kt
@@ -0,0 +1,23 @@
+package com.example.days.domain.resolution.model
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "searchLog")
+class SearchLog(
+    @Column(name = "title", nullable = false)
+    var title: String,
+
+    @JsonSerialize(using = LocalDateTimeSerializer::class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer::class)
+    @Column(name = "createdAt", nullable = false, updatable = false)
+    var createdAt: String
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id : Long? =null
+}
