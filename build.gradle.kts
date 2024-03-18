3c2fe80f72d61e98b07a9302dc3c4cc8ba7c3c38
rim109 <fla7722@naver.com>
2024-03-18 23:56:56 +0900

    ⭐Feat: 배포 테스트 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,6 +70,9 @@ dependencies {
     // postgres
     runtimeOnly("org.postgresql:postgresql")
 
+    //mysql
+//    runtimeOnly ("com.mysql:mysql-connector-j")
+
     implementation ("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     implementation ("com.fasterxml.jackson.core:jackson-databind")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
