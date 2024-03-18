de34926df2c94eb83f945949252645dd9d0c4547
rim109 <fla7722@naver.com>
2024-03-18 22:37:23 +0900

    ⭐Feat: 배포 테스트 10

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-  	runtimeOnly("com.h2database:h2")
+//  	runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
