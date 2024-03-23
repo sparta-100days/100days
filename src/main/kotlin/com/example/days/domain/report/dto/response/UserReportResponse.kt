f2cbf272bbf119d69593932972f8773f97abc7b9
rim109 <fla7722@naver.com>
2024-03-24 00:01:07 +0900

    ⌛Refactor: 신고기능 상태 예외처리 추가

--- src/main/kotlin/com/example/days/domain/report/dto/response/UserReportResponse.kt
+++ src/main/kotlin/com/example/days/domain/report/dto/response/UserReportResponse.kt
@@ -5,15 +5,16 @@ import com.example.days.domain.report.model.UserReport
 data class UserReportResponse(
     val id: Long,
     val reportedUserNickname: String,
+    val reportedUserStatus: String,
     val content: String,
     val reporterNickname: String
 ) {
-    // 신고 당한 사람 닉네임, 신고내용, 신고한 사람 닉네임 (신고 당한 총 수를 넣는 게 맞을까?)
     companion object {
         fun from(userReport: UserReport): UserReportResponse {
             return UserReportResponse(
                 id = userReport.id!!,
                 content = userReport.content,
+                reportedUserStatus = userReport.reportStatus.toString(),
                 reportedUserNickname = userReport.reportedUserId.nickname,
                 reporterNickname = userReport.reporter.nickname
             )
--- src/main/kotlin/com/example/days/domain/report/service/ReportServiceImpl.kt
+++ src/main/kotlin/com/example/days/domain/report/service/ReportServiceImpl.kt
@@ -3,16 +3,15 @@ package com.example.days.domain.report.service
 import com.example.days.domain.report.dto.request.UserReportRequest
 import com.example.days.domain.report.dto.response.UserReportResponse
 import com.example.days.domain.report.model.UserReport
+import com.example.days.domain.report.model.UserReportStatus
 import com.example.days.domain.report.repository.ReportRepository
 import com.example.days.domain.user.model.Status
 import com.example.days.domain.user.repository.UserRepository
-import com.example.days.global.common.exception.common.AlreadyTenReportException
-import com.example.days.global.common.exception.common.ModelNotFoundException
-import com.example.days.global.common.exception.common.NotReportException
-import com.example.days.global.common.exception.common.NotSelfReportException
+import com.example.days.global.common.exception.common.*
 import com.example.days.global.common.exception.user.UserNotFoundException
 import jakarta.transaction.Transactional
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.stereotype.Service
 
 @Service
@@ -33,9 +32,14 @@ class ReportServiceImpl(
             throw NotSelfReportException("본인은 본인을 신고할 수 없습니다.")
         }
 
-        if( reportedUserNickname.countReport >= 10 ){
+        if(reportedUserNickname.countReport >= 10){
             throw AlreadyTenReportException("이 닉네임은 이미 10번 신고 처리되어 밴 처리 진행중입니다.")
         }
+
+        if(req.reportStatus != UserReportStatus.SPAM && req.reportStatus != UserReportStatus.HARMFUL && req.reportStatus != UserReportStatus.ILLEGAL){
+            throw HttpMessageNotReadableException("SPAM, HARMFUL, ILLEGAL 중에 선택해서 입력해주길 바랍니다!")
+        }
+
 
         val report = reportRepository.save(
             UserReport(
@@ -43,7 +47,6 @@ class ReportServiceImpl(
                 reportedUserId = reportedUserNickname,
                 content = req.content,
                 reportStatus = req.reportStatus
-
                 )
         )
         report.reportUser()
