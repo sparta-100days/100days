d1bddc8d47ea8264b5f423eb744e36172522e0fa
rim109 <fla7722@naver.com>
2024-03-27 18:58:56 +0900

    ⌛Refactor: CORS 문제로 인한 로직 수정4

--- src/main/kotlin/com/example/days/global/infra/swagger/SwaggerConfig.kt
+++ src/main/kotlin/com/example/days/global/infra/swagger/SwaggerConfig.kt
@@ -29,7 +29,7 @@ class SwaggerConfig {
                         .name("Authorization")
                 )
             )
-            .addServersItem(Server().url("https://100days.life"))
+            .addServersItem(Server().url("/"))
             .info(
                 Info()
                     .title("100Days API")
