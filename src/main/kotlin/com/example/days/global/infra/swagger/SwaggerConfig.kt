34b7fb13c1048cfea1e38b898336b212e762e2f2
rim109 <fla7722@naver.com>
2024-03-27 18:03:47 +0900

    ⌛Refactor: CORS 문제로 인한 로직 추가

--- src/main/kotlin/com/example/days/global/infra/swagger/SwaggerConfig.kt
+++ src/main/kotlin/com/example/days/global/infra/swagger/SwaggerConfig.kt
@@ -5,39 +5,37 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SwaggerConfig {
 
-    @Configuration
-    class SwaggerConfig {
-
-        @Bean
-        fun openAPI(): OpenAPI {
-            return OpenAPI()
-                .addSecurityItem(
-                    SecurityRequirement().addList("Bearer Authentication")
+    @Bean
+    fun openAPI(): OpenAPI {
+        return OpenAPI()
+            .addServersItem(Server().apply { url("/") })
+            .addSecurityItem(
+                SecurityRequirement().addList("Bearer Authentication")
+            )
+            .components(
+                Components().addSecuritySchemes(
+                    "Bearer Authentication",
+                    SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("Bearer")
+                        .bearerFormat("JWT")
+                        .`in`(SecurityScheme.In.HEADER)
+                        .name("Authorization")
                 )
-                .components(
-                    Components().addSecuritySchemes(
-                        "Bearer Authentication",
-                        SecurityScheme()
-                            .type(SecurityScheme.Type.HTTP)
-                            .scheme("Bearer")
-                            .bearerFormat("JWT")
-                            .`in`(SecurityScheme.In.HEADER)
-                            .name("Authorization")
-                    )
-                )
-                .info(
-                    Info()
-                        .title("100Days API")
-                        .description("100Days API schema")
-                        .version("1.0.0")
-                )
-        }
-
+            )
+            .info(
+                Info()
+                    .title("100Days API")
+                    .description("100Days API schema")
+                    .version("1.0.0")
+            )
     }
+
 }
