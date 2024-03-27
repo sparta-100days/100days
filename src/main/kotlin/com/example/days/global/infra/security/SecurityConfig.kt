33c3799d88ef688bdb0e176f08754c2df087ca59
YJ9352 <act07612@gmail.com>
2024-03-27 11:45:03 +0900

    🏷️Style: SecurityConfig 코드정리

--- src/main/kotlin/com/example/days/global/infra/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/days/global/infra/security/SecurityConfig.kt
@@ -28,25 +28,36 @@ class SecurityConfig(
 //            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
 //            .headers { it.frameOptions { option -> option.disable() } }
             .authorizeHttpRequests {
-                it.requestMatchers(AntPathRequestMatcher("/api/users")).permitAll()
-                it.requestMatchers(AntPathRequestMatcher("/api/admins/**")).permitAll()
-                it.requestMatchers(AntPathRequestMatcher("/api/users/signup")).permitAll()
-                it.requestMatchers(AntPathRequestMatcher("/api/messages/**")).permitAll()
-                it.requestMatchers(AntPathRequestMatcher("/api/reports/**")).permitAll()
-                it.requestMatchers(AntPathRequestMatcher("/api/users/login")).permitAll()
-                it.requestMatchers(AntPathRequestMatcher("/api/users/searchEmail")).permitAll()
-                it.requestMatchers(AntPathRequestMatcher("/api/users/searchPass")).permitAll()
-                it.requestMatchers(AntPathRequestMatcher("/api/users/searchAccountId")).permitAll()
-                it.requestMatchers(AntPathRequestMatcher("/api/mail")).permitAll()
-                it.requestMatchers(AntPathRequestMatcher("/api/mail/sendmail")).permitAll()
-                it.requestMatchers(AntPathRequestMatcher("/api/mail/verifycode")).permitAll()
+                // common
+                it.requestMatchers(AntPathRequestMatcher("/api/mail/**")).permitAll() // mail
+                it.requestMatchers(AntPathRequestMatcher("/api/users/search/**")).permitAll() // search
+                it.requestMatchers(AntPathRequestMatcher("/api/messages/**")).permitAll() // message
+                it.requestMatchers(AntPathRequestMatcher("/api/reports/**")).permitAll() // report
+
+                // user
+                it.requestMatchers(AntPathRequestMatcher("/api/users/login")).permitAll() // login
+                it.requestMatchers(AntPathRequestMatcher("/api/users/signup")).permitAll() // signUp
+
+                // admin
+                it.requestMatchers(AntPathRequestMatcher("/api/admins/**")).permitAll() // login
+
+                // swagger
                 it.requestMatchers(AntPathRequestMatcher("/swagger-ui/**")).permitAll()
                 it.requestMatchers(AntPathRequestMatcher("/v3/api-docs/**")).permitAll()
+
+                // 로그인 임시처리
                 it.requestMatchers(AntPathRequestMatcher("/oauth2/**")).permitAll()
                 it.requestMatchers(AntPathRequestMatcher("**")).permitAll()
-                it.requestMatchers(PathRequest.toH2Console()).permitAll()
-                    .anyRequest().authenticated()
+
+                it.requestMatchers(PathRequest.toH2Console())
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
             }
+            .addFilterBefore(
+                jwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter::class.java
+            )
 //            .oauth2Login { oauthConfig ->
 //                oauthConfig.authorizationEndpoint {
 //                    it.baseUri("/api/v1/oauth2/login")
@@ -56,7 +67,6 @@ class SecurityConfig(
 //                    it.userService(oAuth2UserService)
 //                }.successHandler(oAuth2LoginSuccessHandler)
 //            }
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
             .exceptionHandling {
 //                it.authenticationEntryPoint(authenticationEntryPoint)
 //                it.accessDeniedHandler(accessDeniedHandler)
